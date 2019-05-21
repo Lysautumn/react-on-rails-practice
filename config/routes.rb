56e218c145106966ed65564bdba1a2946fea3ede
Ally <ally@primeacademy.io>
2019-05-21 17:25:30 -0500

    adds react router

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: 'json' } do
+    get 'things', to: 'things#index'
+  end
+  # All requests forwarded to StaticController#index except
+  # Ajax (req.xhr), HTML Mime type (req.format.html?) and root path
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+  # Forward root to StaticController#index
+  root 'static#index'
 end
