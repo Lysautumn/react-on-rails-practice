7cd0c5162289adb17e89301f76268f1b653eedb9
Ally <ally@primeacademy.io>
2019-05-21 17:54:27 -0500

    sending action to reducer

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,32 @@
 import React from "react"
-import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+
+function getThings() {
+  console.log('getThings action');
+  return {
+    type: GET_THINGS_REQUEST
+  };
+}
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+
+        <button className="getThingsButton" onClick={() => this.props.getThings()}>getThings</button>
       </React.Fragment>
     );
   }
 }
 
-HelloWorld.propTypes = {
-  greeting: PropTypes.string
-};
-export default HelloWorld
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+});
+
+const mapDispatchToProps = { getThings };
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
